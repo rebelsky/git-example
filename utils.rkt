32cf20b210fa86bbdc8c37e309e34e61906f6855
Samuel A.Rebelsky <rebelsky@grinnell.edu>
2014-03-29 16:43:37 -0500

    Rename nums to first-n-numbers.

--- utils.rkt
+++ utils.rkt
@@ -8,7 +8,7 @@
 ;;;   Code from the sample lab.
 
 ;;; Procedure:
-;;;   nums
+;;;   first-n-numbers
 ;;; Parameters:
 ;;;   n, a positive integer
 ;;; Purpose:
@@ -21,6 +21,6 @@
 ;;;   (length list-of-nums) == n
 ;;;   (list-ref list-of-nums i) = i+1
 ;;;     for all i, 0 &lt;= i &lt; n.
-(define nums
+(define first-n-numbers
   (lambda (n)
     (cdr (iota (+ n 1)))))
